f1b489a21c43f614852f451ad6055696f60975e1
takayamaki <takayamaki@takayamaki.jp>
2019-06-23 06:50:50 +0900

    大枠をざくっと実装

---
+++ components/page1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page page1">
+    <div class="columns">
+      <div class="column">
+        <copyabelString message="page1" />
+      </div>
+      <div class="column">
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+      </div>
+      <div class="column">
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import copyabelString from '../components/copyableString'
+
+export default {
+  components: {
+    copyabelString,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <div class="page">
-      <copyabelString message="aaa" />
-    </div>
+  <div class="pages">
+    <page1 />
+    <page2 />
+    <page3 />
   </div>
 </template>
 
 <script>
-import copyabelString from '../components/copyableString'
+import page1 from '../components/page1'
+import page2 from '../components/page2'
+import page3 from '../components/page3'
 
 export default {
   components: {
-    copyabelString,
+    page1,
+    page2,
+    page3,
   },
 }
 </script>
