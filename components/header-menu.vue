4e9726c286bc46fa889459fc22d92a5e2d04c1e2
takayamaki <takayamaki@takayamaki.jp>
2019-06-23 09:10:36 +0900

    ヘッダメニューを実装

---
+++ components/header-menu.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a
+        role="button"
+        class="navbar-burger burger"
+        @click="toggleMenuDisplay()"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbar" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          item
+        </a>
+
+        <a class="navbar-item">
+          item
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            item
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              item
+            </a>
+            <a class="navbar-item">
+              item
+            </a>
+            <a class="navbar-item">
+              item
+            </a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item">
+              item
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              item
+            </a>
+            <a class="button is-light">
+              item
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+const toggleMenuDisplay = () => {
+  const menu = document.getElementById('navbar')
+  menu.classList.toggle('is-active')
+}
+
+export default {
+  methods: {
+    toggleMenuDisplay: toggleMenuDisplay,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="pages">
-    <page1 />
-    <page2 />
-    <page3 />
+  <div class="dispaly-area">
+    <headerMenu />
+    <div class="pages">
+      <page1 />
+      <page2 />
+      <page3 />
+    </div>
   </div>
 </template>
 
 <script>
+import headerMenu from '../components/header-menu'
 import page1 from '../components/page1'
 import page2 from '../components/page2'
 import page3 from '../components/page3'
 
 export default {
   components: {
+    headerMenu,
     page1,
     page2,
     page3,
