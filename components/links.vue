13df8e5c479c5c2d82fc23207a10039af6dcd6a2
takayamaki <takayamaki@takayamaki.jp>
2019-06-23 15:11:05 +0900

    links.vueを作った

--- components/links.vue
+++ components/links.vue
@@ -1,31 +1,42 @@
 <template>
   <div id="links" class="page">
-    <div class="columns">
-      <div class="column">
-        <copyabelString message="page1" />
-      </div>
-      <div class="column">
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-      </div>
-      <div class="column">
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
-        これは仮の内容 これは仮の内容 これは仮の内容 これは仮の内容
+    <div class="container">
+      <div class="content is-small">
+        <h2 class="title">
+          Links
+        </h2>
+        <div class="columns">
+          <div class="column">
+            <a href="https://twitter.com/fusagiko">
+              <fa-icon :icon="['fab', 'twitter']" size="5x" />
+              <h3>Twitter</h3>
+            </a>
+          </div>
+
+          <div class="column">
+            <a href="https://github.com/takayamaki">
+              <fa-icon :icon="['fab', 'github']" size="5x" />
+              <h3>GitHub</h3>
+            </a>
+          </div>
+          <div class="column">
+            <a href="https://imastodon.net/@fusagiko">
+              <fa-icon :icon="['fab', 'mastodon']" size="5x" />
+              <h3>Mastodon</h3>
+            </a>
+          </div>
+          <div class="column">
+            <a href="https://fusagiko.hatenablog.jp/">
+              <fa-icon icon="edit" size="5x" />
+              <h3>Blog</h3>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import copyabelString from '../components/copyableString'
-
-export default {
-  components: {
-    copyabelString,
-  },
-}
+export default {}
 </script>
--- plugins/FontAwesome.js
+++ plugins/FontAwesome.js
@@ -4,12 +4,17 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faClipboard,
   faExternalLinkAlt,
+  faEdit,
 } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faTwitter,
+  faGithub,
+  faMastodon,
+} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faClipboard, faExternalLinkAlt)
-library.add(faTwitter, faGithub)
+library.add(faClipboard, faExternalLinkAlt, faEdit)
+library.add(faTwitter, faGithub, faMastodon)
 
 Vue.component('fa-icon', FontAwesomeIcon)
 
